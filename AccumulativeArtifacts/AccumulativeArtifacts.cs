9f858a9dd1ea491238feba52153ade99706d6ec7
KingEnderBrine <31794213+KingEnderBrine@users.noreply.github.com>
2020-07-13 22:10:09 +0700

    Release 1.0.0

---
+++ AccumulativeArtifacts/AccumulativeArtifacts.cs
@@ -0,0 +1,63 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using R2API.Utils;
+using RoR2;
+using System.Linq;
+using UnityEngine;
+
+namespace ArtifactsRandomizer
+{
+    [R2APISubmoduleDependency(nameof(CommandHelper))]
+    [BepInDependency("com.bepis.r2api")]
+    [BepInPlugin("com.KingEnderBrine.AccumulativeArtifacts", "Accumulative Artifacts", "1.0.0")]
+    public class ArtifactsRandomizerPlugin : BaseUnityPlugin
+    {
+        private static ConfigWrapper<bool> isEnabled { get; set; }
+
+        public void Awake()
+        {
+            CommandHelper.AddToConsoleWhenReady();
+
+            isEnabled = Config.Wrap("Main", "enabled", "Is mod should accumulate artifacts or not", true);
+
+            On.RoR2.ArtifactTrialMissionController.OnCurrentArtifactDiscovered += (orig, self, artifactDef) =>
+            {
+                orig(self, artifactDef);
+                if (isEnabled.Value)
+                {
+                    self.SetFieldValue("artifactWasEnabled", true);
+                }
+            };
+        }
+
+        [ConCommand(commandName = "aa_enable", flags = ConVarFlags.None, helpText = "Enable artifacts accumulation")]
+        private static void CCEnable(ConCommandArgs args)
+        {
+            if (isEnabled.Value)
+            {
+                return;
+            }
+            isEnabled.Value = true;
+            isEnabled.ConfigFile.Save();
+            Debug.Log($"[AccumulativeArtifacts] is enabled");
+        }
+
+        [ConCommand(commandName = "aa_disable", flags = ConVarFlags.None, helpText = "Disable artifacts accumulation")]
+        private static void CCDisable(ConCommandArgs args)
+        {
+            if (!isEnabled.Value)
+            {
+                return;
+            }
+            isEnabled.Value = false;
+            isEnabled.ConfigFile.Save();
+            Debug.Log($"[AccumulativeArtifacts] is disabled");
+        }
+
+        [ConCommand(commandName = "aa_status", flags = ConVarFlags.None, helpText = "Shows is artifacts accumulation enabled or disabled")]
+        private static void CCStatus(ConCommandArgs args)
+        {
+            Debug.Log($"[AccumulativeArtifacts] is {(isEnabled.Value ? "enabled" : "disabled")}");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,16 @@
+# Description
+Everytime you collect an artifact via trial in `Bulwark's Ambry`, it sticks throughout the run.
+
+# Console commands
+
+* `aa_status` - Shows is artifacts accumulation enabled or disabled
+
+* `aa_disable` - Disable artifacts accumulation. Rememebers status even if game is restarted.
+
+* `aa_enable` - Enable artifacts accumulation.
+
+***
+# Changelog
+**1.0.0**
+
+* Mod release.
