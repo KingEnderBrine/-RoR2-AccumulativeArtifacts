ad6a758cdfc22f3a43bebf71de922d84fb94fd6e
KingEnderBrine <31794213+KingEnderBrine@users.noreply.github.com>
2020-11-20 21:02:30 +0700

    Mod release 1.1.0

---
+++ AccumulativeArtifacts/AccumulativeArtifactsPlugin.cs
@@ -0,0 +1,58 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using InLobbyConfig;
+using R2API.Utils;
+using RoR2;
+using System.Security;
+using System.Security.Permissions;
+
+[module: UnverifiableCode]
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+namespace AccumulativeArtifacts
+{
+    [NetworkCompatibility(CompatibilityLevel.NoNeedForSync)]
+    [BepInDependency("com.KingEnderBrine.InLobbyConfig")]
+    [BepInDependency("com.bepis.r2api")]
+    [BepInPlugin("com.KingEnderBrine.AccumulativeArtifacts", "Accumulative Artifacts", "1.1.0")]
+    public class AccumulativeArtifactsPlugin : BaseUnityPlugin
+    {
+        internal static AccumulativeArtifactsPlugin Instance { get; private set; }
+        internal static ConfigEntry<bool> IsEnabled { get; set; }
+        private ModConfigEntry ModConfig { get; set; }
+
+        public void Awake()
+        {
+            Instance = this;
+
+            IsEnabled = Config.Bind("Main", "enabled", true, "Should accumulate artifacts or not?");
+            ModConfig = new ModConfigEntry
+            {
+                DisplayName = "Accumulative Artifacts",
+                EnableField = new InLobbyConfig.Fields.BooleanConfigField("", () => IsEnabled.Value, (newValue) => IsEnabled.Value = newValue)
+            };
+
+            ModConfigCatalog.Add(ModConfig);
+
+            On.RoR2.ArtifactTrialMissionController.OnCurrentArtifactDiscovered += OnCurrentArtifactDiscovered;
+        }
+
+        public void Destroy()
+        {
+            Instance = null;
+
+            ModConfigCatalog.Remove(ModConfig);
+
+            On.RoR2.ArtifactTrialMissionController.OnCurrentArtifactDiscovered -= OnCurrentArtifactDiscovered;
+        }
+
+        private static void OnCurrentArtifactDiscovered(On.RoR2.ArtifactTrialMissionController.orig_OnCurrentArtifactDiscovered orig, ArtifactTrialMissionController self, ArtifactDef artifactDef)
+        {
+            orig(self, artifactDef);
+
+            if (IsEnabled.Value)
+            {
+                self.artifactWasEnabled = true;
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,16 +1,16 @@
 # Description
 Everytime you collect an artifact via trial in `Bulwark's Ambry`, it sticks throughout the run.
 
-# Console commands
-
-* `aa_status` - Shows is artifacts accumulation enabled or disabled
-
-* `aa_disable` - Disable artifacts accumulation. Rememebers status even if game is restarted.
-
-* `aa_enable` - Enable artifacts accumulation.
-
 ***
 # Changelog
+**1.1.0**
+
+* Using `InLobbyConfig` to enable/disable mod instead of console commands
+
+**1.0.1**
+
+* Marked as client side mod.
+
 **1.0.0**
 
 * Mod release.
